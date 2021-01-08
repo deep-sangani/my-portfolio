24495bcdd9cd0d09b0251d12ad5337478c19216a
deep sangani <deep.r.sangani@gmail.com>
2021-01-08 13:48:05 +0530

    add overlay and moreinfo on project page

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,33 +2,35 @@ import React, { useState } from 'react'
 import "./Card.css"
 
 
-export default function Card({project,setpassproject}) {
-    
+export default function Card({ project, setpassproject }) {
 
-  const handleclick=(e)=>{
-      setpassproject(project)
-     
-  }
-   
-        return (
-            <>
-           
+
+    const handleclick = (e) => {
+        setpassproject(project)
+
+    }
+
+    return (
+        <>
+
             <div className="card">
+                <div className="card-overlay" onClick={e => handleclick(e)}>
+                    <div className="middle">
+                    <h3> Click for More info</h3>
+                </div></div>
                 <div className="top">
-                    <img src={project.img} alt=""  onClick={e=>handleclick(e)} />
+                    <img src={project.img} alt="" />
                 </div>
-                  <div >
-                  <div className="middle">
-                        <a href={project.link}>Github Link</a>
-                    </div>
+                <div >
+
                     <div className="bottom">
-                        <p>{ project.desc}  </p>
+                        <p>{project.desc}  </p>
                     </div>
-                  </div>
+                </div>
             </div>
-            </>
-        )
-    
+        </>
+    )
+
 
 
 }
--- src/components/ProjectImgs.js
+++ src/components/ProjectImgs.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import "./Projectimgs.css"
 
+
+function CardProject({passproject}){
+   return(
+      <div className="cardproject">
+         <h2>Home Page</h2>
+         <img src={passproject.img} alt=""/>
+         <a href={passproject.link}>GitHub link</a>
+            <div className="desc">
+               <h3>{passproject.desc}</h3>
+            </div>
+      </div>
+   )
+}
+
 export default function ProjectImgs({passproject,setpassproject}) {
     return (
         <div className="projectimgs" onClick={()=>setpassproject(null)}>
            <div className="wrapper">
            <h1>Real-time Pizza App</h1>
-            <img src={passproject.img} alt=""/>
-            <div className="desc">
-               <h3>{passproject.desc}</h3>
-            </div>
-            <img src={passproject.img} alt=""/>
-            <div className="desc">
-               <h3>{passproject.desc}</h3>
-            </div>
-            <img src={passproject.img} alt=""/>
-            <div className="desc">
-               <h3>{passproject.desc}</h3>
-            </div>
-            <img src={passproject.img} alt=""/>
-            <div className="desc">
-               <h3>{passproject.desc}</h3>
-            </div>
+           <CardProject passproject={passproject}/>
+           <CardProject passproject={passproject}/>
+            
+           
            </div>
             
         </div>
